41cb113263e84e892150584681a9e87cdddc6699
alexkychen <alexkychen@gmail.com>
2016-09-29 22:12:54 -0400

    test

---
+++ printTemp.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#Read four temperature probes (DS18B20) and print temperature to LCD display
+
+import time
+import LCD_driver
+
+#Read sensor SN file (ProbeSN.txt)
+try:
+	openfile = open("ProbeSN.txt","r")
+except:
+	print("Oops. Can't find probe serial numbers. Check if ProbeSN.txt exists.")
+
+#Extract probe SN from ProbeSN.txt file
+SensorSN_list = []
+for line in openfile:
+	SensorSN_list.append(line)
+
+print(SensorSN_list)
+
+
+
+#def readTemp
