f23efd91cc9d9b10631012cb4a28ffede0eb3677
alexkychen <alexkychen@gmail.com>
2016-09-29 22:52:50 -0400

    edit printTemp

--- printTemp.py
+++ printTemp.py
@@ -4,6 +4,12 @@
 import time
 import LCD_driver
 
+#Set up LCD display
+display = LCD_driver.lcd()
+display.lcd_clear()
+display.lcd_display_string("Reading",1)
+display.lcd_display_string("temperatures...",2)
+
 #Read sensor SN file (ProbeSN.txt)
 try:
 	openfile = open("ProbeSN.txt","r")
@@ -16,8 +22,34 @@ for line in openfile:
 	line = line.strip()
 	SensorSN_list.append(line)
 
-print(SensorSN_list)
+#Read temperature from each sensor/probe
+def readTemp():
+	TempData = []
+	base_dir = '/sys/bus/w1/devices/'
+	for sensorSN in SensorSN_list:
+		sensor_file = base_dir + sensorSN + '/w1_slave'
+		sensor_open = open(sensor_file, 'r')
+		sensor_read = sensor_open.read()
+		sensor_open.close()
+		#convert temp reading to C degree
+		temp_string = sensor_read.split('\n')[1].split(' ')[9]
+		temp_num = float(temp_string[2:])/1000
+		temp_num = round(temp_num,1)
+		TempData.append(temp_num)
+	row1 = "#1:"+str(TempData[0])+" #2:"+str(TempData[1])
+	row2 = "#3:"+str(TempData[2])+" #4:"+str(TempData[3])
+	display.lcd_display_string(row1,1)
+	display.lcd_display_string(row2,2)
 
 
+#Print temperatures on LCD display
+display.lcd_clear()
+try:
+	while True:
+		readTemp()
+		time.sleep(3)
+		display.lcd_clear()
+except KeyboardInterrupt:
+	display.lcd_clear()
 
-#def readTemp
+
